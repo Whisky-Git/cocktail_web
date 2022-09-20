56ffaf5625fca5bc9be8a92ffd8131ab46b9f4b7
Sangjoon Lee <openjoon@gamail.com>
2022-09-20 19:27:45 +0900

    테스트

--- spring_final/src/main/java/com/spring/controller/AdminController.java
+++ spring_final/src/main/java/com/spring/controller/AdminController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
-	private static final Logger Logger = LoggerFactory.getLogger(AdminController.class);
+	private static final Logger logger = LoggerFactory.getLogger(AdminController.class);
 	
 	//관리자 메인 페이지 이동
 	@RequestMapping(value="main", method = RequestMethod.GET)
@@ -18,6 +18,30 @@ public class AdminController {
 		
 		System.out.println("관리자 페이지 이동");
 	}
+	
+	/* 상품 등록 페이지 접속 */
+    @RequestMapping(value = "goodsManage", method = RequestMethod.GET)
+    public void goodsManageGET() throws Exception{
+        logger.info("상품 등록 페이지 접속");
+    }
+    
+    /* 상품 등록 페이지 접속 */
+    @RequestMapping(value = "goodsEnroll", method = RequestMethod.GET)
+    public void goodsEnrollGET() throws Exception{
+        logger.info("상품 등록 페이지 접속");
+    }
+    
+    /* 작가 등록 페이지 접속 */
+    @RequestMapping(value = "authorEnroll", method = RequestMethod.GET)
+    public void authorEnrollGET() throws Exception{
+        logger.info("작가 등록 페이지 접속");
+    }
+    
+    /* 작가 관리 페이지 접속 */
+    @RequestMapping(value = "authorManage", method = RequestMethod.GET)
+    public void authorManageGET() throws Exception{
+        logger.info("작가 관리 페이지 접속");
+    } 
 		
 	
 	 
